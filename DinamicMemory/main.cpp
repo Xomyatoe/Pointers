f3eee294c26193067b533f2b52de782df6802017
Seva <Seva@DESKTOP-PLC715R>
2024-05-13 20:51:18 +0500

    push_row_back

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -1,21 +1,142 @@
 ﻿#include <iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 #define tab "\t"
-void FillRand(int arr[], const int n);
+
+int** Allocate(const int rows, const int cols);
+void Clear(int** arr, const int rows);
+
+void FillRand(int arr[], const int n,int minRand = 0, int maxRand = 100);
+void FillRand(int** arr, const int rows, const int cols);
 void Print(int arr[], const int n);
+void Print(int** arr, const int rows, const int cols);
+
+int* push_back(int arr[], int& n, int value);
+int* push_front(int arr[], int& n, int value);
+int* pop_back(int arr[], int& n);
+
+int** push_row_back(int** arr,  int& rows, const int cols);
+
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+#ifdef DYNAMIC_MEMORY_1
 	int n;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
-	
+
 	FillRand(arr, n);
 	Print(arr, n);
 
 	int value;
 	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+	delete[] arr;
+
+#endif // DYNAMIC_MEMORY_1
+
+#ifdef DYNAMIC_MEMORY_2
+	int rows;
+	int cols;
+	cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элементов строк: "; cin >> cols;
+
+	
+
+	int** arr = Allocate(rows, cols);
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+	arr = push_row_back(arr, rows, cols);
+	FillRand(arr[rows - 1], cols, 100,1000);
+	Print(arr, rows, cols);
+
+	Clear(arr, rows);
+
+#endif
+
+}
+
+int** Allocate(const int rows, const int cols)
+{
+	//1) создаем массив указателей
+	int** arr = new int* [rows];
+	//2) выделяем память под строки
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols] {};
+	}
+	return arr;
+}
+void Clear(int** arr, const int rows)
+{
+	//3)Сначала удаляем строки
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	//4) удаляем массив указателей
+	delete[] arr;
+}
+void FillRand(int arr[], const int n,int minRand,int maxRand)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//обращение к элементам через арифметику указателей
+		//и оператор разыменования:
+		*(arr + i) = rand() % (maxRand - minRand) + minRand;
+
+	}
+}
+void FillRand(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//обращение к элементам через оператор индексирования -[];
+		cout << arr[i] << tab;
+
+	}
+	cout << endl;
+}
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int* push_back(int arr[], int& n, int value)
+{
 	//1)Создаем буферный массив нужного размера
 	int* buffer = new int[n + 1];
 	//2)Копируем значемя из исходного массива в буферный
@@ -31,28 +152,41 @@ void main()
 	//куда можно добавить значение
 	arr[n] = value;
 	n++;
-	Print(arr, n);
-	
+	//Print(arr, n);
+	return arr;
+}
+int* push_front(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
 	delete[] arr;
-
+	buffer[0] = value;
+	n++;
+	return buffer;
 }
-void FillRand(int arr[], const int n)
+int* pop_back(int arr[], int& n)
 {
-	for (int i = 0; i < n; i++)
-	{
-		//обращение к элементам через арифметику указателей
-		//и оператор разыменования:
-		*(arr + i) = rand() % 100;
-
-	}
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
 }
-void Print(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		//обращение к элементам через оператор индексирования -[];
-		cout << arr[i] << tab;
 
-	}
-	cout << endl;
+int** push_row_back(int** arr,  int& rows, const int cols)
+{
+	//создаем буферный массив указателей нужного размера
+	int** buffer = new int* [rows+1];
+
+	//Копируем строки из исходного массивав массив указателей
+	for (int i = 0; i < rows; i++)buffer[i] = arr[i];
+	//удаляем исходный массив указателей
+	delete[] arr;
+	//создаем строку и добавляем ее в массив
+	buffer[rows] = new int[cols] {};
+	//После добавления строки в массив колличество его строк увеличивается
+	rows++;
+	return buffer;
 }
